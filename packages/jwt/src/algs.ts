9debd5470219b2ab8fff50086584af6ef838fab7
Stuart Low <stuart@biza.io>
2022-05-09 23:18:04 +1000

    Add signing components

--- packages/jwt/src/algs.ts
+++ packages/jwt/src/algs.ts
@@ -5,3 +5,24 @@ export const algToHash: Record<string, string> = {
 };
 
 export const algs = Object.keys(algToHash);
+
+/**
+ * "Next Gen" algorithms map, need to refactor old alg stuff above
+ */
+export const algorithms: {
+  [key: string]:
+    | AlgorithmIdentifier
+    | RsaPssParams
+    | EcdsaParams
+    | EcKeyImportParams;
+} = {
+  ES256: { name: 'ECDSA', namedCurve: 'P-256', hash: { name: 'SHA-256' } },
+  ES384: { name: 'ECDSA', namedCurve: 'P-384', hash: { name: 'SHA-384' } },
+  ES512: { name: 'ECDSA', namedCurve: 'P-512', hash: { name: 'SHA-512' } },
+  HS256: { name: 'HMAC', hash: { name: 'SHA-256' } },
+  HS384: { name: 'HMAC', hash: { name: 'SHA-384' } },
+  HS512: { name: 'HMAC', hash: { name: 'SHA-512' } },
+  RS256: { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-256' } },
+  RS384: { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-384' } },
+  RS512: { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' } }
+};
---
+++ packages/jwt/src/sign.ts
@@ -0,0 +1,44 @@
+import { algorithms } from './algs';
+import { base64url } from 'rfc4648';
+
+/**
+ * At the moment this is really only targeted at support HMAC secret key based signing
+ * @param payload
+ * @param secret
+ * @param alg
+ */
+export async function signJwt(
+  payload: { [k: string]: any },
+  secret: string,
+  alg: string
+): Promise<String> {
+  const importAlgorithm = algorithms['alg'];
+  if (!importAlgorithm) throw new Error('algorithm not found');
+  payload.iat = Math.floor(Date.now() / 1000);
+  const payloadAsJSON = JSON.stringify(payload);
+  const partialToken = `${base64url.stringify(
+    _utf8ToUint8Array(
+      JSON.stringify({
+        alg: alg
+      })
+    )
+  )}.${base64url.stringify(_utf8ToUint8Array(payloadAsJSON))}`;
+  const keyData = _utf8ToUint8Array(secret);
+  const key = await crypto.subtle.importKey(
+    'raw',
+    keyData,
+    importAlgorithm,
+    false,
+    ['sign']
+  );
+  const signature = await crypto.subtle.sign(
+    importAlgorithm,
+    key,
+    _utf8ToUint8Array(partialToken)
+  );
+  return `${partialToken}.${base64url.stringify(new Uint8Array(signature))}`;
+}
+
+function _utf8ToUint8Array(str: string) {
+  return base64url.parse(btoa(decodeURI(encodeURIComponent(str))));
+}
