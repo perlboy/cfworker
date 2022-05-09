f24b5d5afd0eedc9096b29ce32aecc428dc75e1f
Stuart Low <stuart@biza.io>
2022-05-10 00:54:03 +1000

    Export new functions

--- packages/jwt/src/index.ts
+++ packages/jwt/src/index.ts
@@ -3,3 +3,5 @@ export { getJwks, getKey, importKey } from './jwks.js';
 export { parseJwt } from './parse.js';
 export * from './types.js';
 export { verifyJwtSignature } from './verify.js';
+export { signJwt } from './sign.js';
+export { getIssuerMetadata } from './discovery.js';
